a38ffed86488ca0e11647680981d34d653efb898
Mika Korpela <mika.1.korpela@nokia.com>
2019-02-01 16:04:20 +0200

    Commit

---
+++ reactjs-diet/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class Footer extends React.Component
+{
+    render()
+    {
+        return (
+            <div class="Footer">This is the footer</div>
+        );
+    }
+}
+
+export default Footer;
---
+++ reactjs-diet/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+class Header extends React.Component
+{
+    render()
+    {
+        return (
+            <div class="Header">This is the header</div>
+        );
+    }
+}
+
+export default Header;
